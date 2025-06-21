5a42c787c9be00bccec42ddec9fdb0928afbf4bd
Mahin <mahindipto18@gmail.com>
2025-06-21 17:13:30 +0600

    created login page ui eith necessery fileds

--- src/javabank/FXMLDocumentController.java
+++ src/javabank/FXMLDocumentController.java
@@ -9,26 +9,34 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  *
  * @author User
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private ImageView logoImageView;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private TextField login_acnt_field;
+    @FXML
+    private PasswordField login_pass_field;
+    @FXML
+    private Button loginBtn;
+    @FXML
+    private Button regBtn;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        Image logo = new Image(getClass().getResourceAsStream("/javabank/assets/banklogo.png"));
+        logoImageView.setImage(logo);
+    }
+
 }
