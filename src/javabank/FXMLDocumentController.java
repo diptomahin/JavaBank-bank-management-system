ecd9a99f5fee24fb2d8cf02a29f1a36946a2c5b6
Mahin <mahindipto18@gmail.com>
2025-06-22 20:36:09 +0600

    added new scene for user ragistration

--- src/javabank/FXMLDocumentController.java
+++ src/javabank/FXMLDocumentController.java
@@ -4,17 +4,22 @@
  */
 package javabank;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 /**
  *
@@ -37,6 +42,24 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         Image logo = new Image(getClass().getResourceAsStream("/javabank/assets/banklogo.png"));
         logoImageView.setImage(logo);
+    }
+
+    @FXML
+    private void handleRegBtn(ActionEvent event) {
+         try {
+        // Load the FXML for the new scene
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("registrationForm.fxml"));
+        Parent root = loader.load();
+
+        // Get current stage and set the new scene
+        Stage stage = (Stage) regBtn.getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Register");
+        stage.show();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
     }
 
 }
