cbe09b9be8cd7f4230a8e9b43e39034627e0568a
Mahin <mahindipto18@gmail.com>
2025-06-23 20:57:24 +0600

    completed registatio Form UI

--- src/javabank/RegistrationFormController.java
+++ src/javabank/RegistrationFormController.java
@@ -6,7 +6,15 @@ package javabank;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -15,12 +23,36 @@ import javafx.fxml.Initializable;
  */
 public class RegistrationFormController implements Initializable {
 
+    @FXML
+    private TextField reg_namefield;
+    @FXML
+    private TextField reg_emailfield;
+    @FXML
+    private TextField reg_phonefield;
+    @FXML
+    private RadioButton male;
+    @FXML
+    private ToggleGroup reg_gender;
+    @FXML
+    private RadioButton others;
+    @FXML
+    private RadioButton female;
+    @FXML
+    private DatePicker reg_datefiled;
+    @FXML
+    private TextArea reg_streetAddress;
+    @FXML
+    private TextField reg_thana;
+    @FXML
+    private TextField reg_district;
+
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+    }
+
 }
